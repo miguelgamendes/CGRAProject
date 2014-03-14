12f8ac0fdf2469ba9d351935eca0d4ec21624034
Diogobarroso <ei11105@fe.up.pt>
2014-03-14 12:09:19 +0000

    tp3 added. Nothing has been done, you lazy dumbass.

---
+++ 3rd class/src/myCylinder.h
@@ -0,0 +1,15 @@
+#ifndef MY_CYLINDER
+#define MY_CYLINDER
+
+#include "CGF/CGFobject.h"
+
+class myCylinder : public CGFobject {
+
+	public:
+		myCylinder(int slices, int stacks/*, bool smooth*/);
+
+};
+
+
+
+#endif
